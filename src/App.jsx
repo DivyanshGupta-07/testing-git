4bd7dc47da663bc251809bebeeec3265772e9e0e
Divyansh Gupta <divyanshg2000@gmail.com>
2024-10-10 20:14:16 +0530

    final commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div>
+      <h1>hi</h1>
+      <h1>my</h1>
+      <h1>name</h1>
+      <h1>is</h1>
+      <h1>Divyansh</h1>
+    </div>
+  );
+};
+
+export default App;
